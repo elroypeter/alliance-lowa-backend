22c89c6b7aba2f0f3eb41416ae90b5029267fbdb
elroypeter <30926117+elroypeter@users.noreply.github.com>
2022-09-19 14:29:46 +0300

    updated deployment scripts

--- src/controller/Auth.controller.ts
+++ src/controller/Auth.controller.ts
@@ -27,7 +27,7 @@ class AuthController {
                 const token = sign(
                     {
                         userId: user.id,
-                        email,
+                        email: email,
                         date: new Date().getTime(),
                     },
                     config.jwt_secret,
