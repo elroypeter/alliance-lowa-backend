5843e4731fb3d3a4c465d6a05c6d5fdca8035c45
elroypeter <30926117+elroypeter@users.noreply.github.com>
2022-10-17 17:55:52 +0300

    Added more authentication tests

--- src/tests/auth/auth.test.ts
+++ src/tests/auth/auth.test.ts
@@ -1,68 +1,50 @@
 import { App } from '../../bootstrap';
 import * as Request from 'supertest';
-import { IMemoryDb, newDb } from 'pg-mem';
-import { configService } from '../../config';
-import { RegisteredEntities } from '../../entity';
+import * as bcrypt from 'bcrypt';
 import { ResponseCode } from '../../enums/response.enums';
-import { DataSource } from 'typeorm';
+import { TestAppInstance } from '../setup';
+import { UserEntity } from '../../entity/User.entity';
 
 describe('Authentication', () => {
   let TestApp: App;
-  let TestDB: IMemoryDb;
-  let TestDataSource: DataSource;
 
   beforeAll(async () => {
-    TestDB = newDb({
-      autoCreateForeignKeyIndices: true,
-    });
+    TestApp = await TestAppInstance();
 
-    TestDB.public.registerFunction({
-      implementation: () => 'test',
-      name: 'current_database',
-    });
-
-    TestDB.public.registerFunction({
-      implementation: () => 'test',
-      name: 'version',
-    });
-
-    TestDB.public.interceptQueries((queryText) => {
-      if (
-        queryText.search(
-          /(pg_views|pg_matviews|pg_tables|pg_enum|columns.*|ALTER TABLE)/g,
-        ) > -1
-      ) {
-        return [];
-      }
-      return null;
-    });
-
-    TestDataSource = await TestDB.adapters.createTypeormDataSource({
-      type: 'postgres',
-      database: ':memory:',
-      dropSchema: true,
-      entities: RegisteredEntities,
-      synchronize: true,
-      logging: false,
-    });
-
-    TestDataSource = await TestDataSource.initialize();
-
-    TestApp = await new App(
-      configService(RegisteredEntities, '../env.test.yml'),
-      TestDataSource,
-    ).initApp({ enableKoaLogger: false });
-  });
-
-  it('fails unknown user sign in', async () => {
     const email = 'test@gmail.com';
     const password = 'test';
+    const hashPassword = await bcrypt.hash(password, 10);
 
+    const user = UserEntity.create({
+      email,
+      password: hashPassword,
+      name: 'John',
+    });
+    await user.save();
+  });
+
+  it('unknown user fails to sign-in', async () => {
     const response = await Request(TestApp.koaInstance.callback())
       .post('/login')
-      .send({ email, password });
+      .send({ email: 'unknown@gmail.com', password: 'test' });
 
     expect(response.status).toEqual(ResponseCode.UNAUTHORIZED);
+    expect(response.text).toEqual('User with email not found');
+  });
+
+  it('user signs in successfully', async () => {
+    const response = await Request(TestApp.koaInstance.callback())
+      .post('/login')
+      .send({ email: 'test@gmail.com', password: 'test' });
+    expect(response.status).toEqual(ResponseCode.OK);
+  });
+
+  it('user cannot sign in with invalid password', async () => {
+    const response = await Request(TestApp.koaInstance.callback())
+      .post('/login')
+      .send({ email: 'test@gmail.com', password: '2test2' });
+    expect(response.status).toEqual(ResponseCode.UNAUTHORIZED);
+    expect(response.text).toEqual('Invalid user password');
   });
 
   afterAll(() => {
